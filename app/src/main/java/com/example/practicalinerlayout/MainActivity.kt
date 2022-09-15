8dd06c2f88ca6b0b604c50a90b70e0731d4321bc
LeJammes454 <s19120178@alumnos.itsur.edu.mx>
2022-09-14 22:40:52 -0500

    Parte 2 adjunte el detector de clics al botón HECHO

--- app/src/main/java/com/example/practicalinerlayout/MainActivity.kt
+++ app/src/main/java/com/example/practicalinerlayout/MainActivity.kt
@@ -2,15 +2,37 @@ package com.example.practicalinerlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener{
+            addNickName(it)
+        }
     }
 
-    fun clickHandlerFunction(view: View) {}
+    private fun addNickName(view : View){
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
 
+        val  addNickName = findViewById<Button>(R.id.done_button)
+
+        nicknameTextView.text=editText.text
+        editText.visibility= View.GONE
+        view.visibility=View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+
+        addNickName.setOnClickListener{
+            Log.d("MainActivity","sad")
+        }
+    }
 
 }
